e3dfdff319a463ec1e3bb925ee7a8ac19d3288c4
Manel Guechetouli <manelguechetouli@gmail.com>
2026-01-21 10:40:05 -0500

    [ADD] clinique_veterinaire : noueau module

---
+++ clinique_veterinaire/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Clinique veterinaire',
+    'version': '1.0',
+    'summary': 'Gestion d\'une file d\'animaux',
+    'category': 'Services',
+    'author': 'Manel',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
