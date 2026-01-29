73cdb5260a9bcd1b3b89cc0d9742129f9fd7b5a0
Manel Guechetouli <manelguechetouli@gmail.com>
2026-01-29 09:07:30 -0500

    [ADD] debut options snippet

--- clinique_veterinaire/static/src/js/clinique_snippet_options.esm.js
+++ clinique_veterinaire/static/src/js/clinique_snippet_options.esm.js
@@ -2,6 +2,6 @@
 
 import options from "@web_editor/js/editor/snippets.options";
 
-options.registry.clinique_animaux = options.Class.extend({
+options.registry.js_clinique_animaux = options.Class.extend({
 
 });
